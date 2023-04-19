62f4c833d35694b44495af59b0a29d238d2bccdd
emili <emili@DESKTOP-GILPLCP>
2023-04-19 12:12:45 +0200

    usuario sin @

--- src/MiClase.java
+++ src/MiClase.java
@@ -3,23 +3,8 @@ public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("inicio");
-		String asignaturas= "Entornos";
-		System.out.println("1");
-<<<<<<< HEAD
-		System.out.println("2");
-
-		System.out.println("3");
-
-		System.out.println("Rama 1");
-		System.out.println("Rama 1.1");
-		System.out.println("Rama 1.2");
-
-=======
-		System.out.println("Rama1");
-		System.out.println("Rama 1.1");
-		System.out.println("Rama 1.2");
->>>>>>> refs/heads/Rama1
+		System.out.println("usuario sin @");
+		
 	}
 
 }
