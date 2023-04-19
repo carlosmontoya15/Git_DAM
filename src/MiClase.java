00e88645fd0c9161ce9df4cf572819c0e62739ef
emili <emili@DESKTOP-GILPLCP>
2023-04-19 13:15:12 +0200

    al subir la primera vez en una sesión de eclipes

--- src/MiClase.java
+++ src/MiClase.java
@@ -4,7 +4,7 @@ public class MiClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("subir a GitHub");
-		
+		System.out.println("al subir la primera vez en una sesión");
 	}
 
 }
