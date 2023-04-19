0685b955cb00cb429443c05c539a5dee90febe03
emili <emili@DESKTOP-GILPLCP>
2023-04-19 13:07:04 +0200

    subir a GitHub

--- src/MiClase.java
+++ src/MiClase.java
@@ -3,7 +3,7 @@ public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("usuario sin @");
+		System.out.println("subir a GitHub");
 		
 	}
 
